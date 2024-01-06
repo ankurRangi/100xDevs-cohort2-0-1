711d80b6abe4c82d265bcf071200033d78cccfc4
Ankur <ankur.rangi08@gmail.com>
2024-01-06 22:50:59 +0530

    Week 06 Added

---
+++ ClassQues/W06/Class 01/experiments/src/App.jsx
@@ -0,0 +1,50 @@
+import { Children, Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <CardWrapper>
+      Hi there
+    </CardWrapper>
+  )
+}
+
+function CardWrapper({children}) {
+  return <div style={{border: "2px solid black", padding: 20, margi: 10}}>
+    {children}
+  </div>
+}
+
+// function Todo({title, description}){
+//   return <div>
+//     <h1>
+//       {title}
+//     </h1>
+//     <h5>
+//       {description}
+//     </h5>
+//   </div>
+// }
+
+// function HeaderWithButtons(){
+//   const [title, setTitle] = useState("My Name is Ankur")
+
+//   function updateTitle(){
+//     setTitle("My name is " + Math.random());
+//   }
+
+//   return <div>
+//       <button onClick={updateTitle}>Update the title</button>
+//       <Header title={title}></Header>
+//   </div>
+// }
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
